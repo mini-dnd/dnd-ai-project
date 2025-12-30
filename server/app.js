df1c736db1969c395db271e879662acd207f52b7
sabianathallah <sabian.athallah05@gmail.com>
2025-12-30 13:34:49 +0700

    last update 30th dec 2025

--- server/app.js
+++ server/app.js
@@ -577,10 +577,6 @@ Only return the JSON, nothing else.`;
   });
 });
 
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 httpServer.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
